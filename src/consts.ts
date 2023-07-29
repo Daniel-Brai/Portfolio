851d7ec423accd442f8a5ed802a526874fab2b69
Daniel Brai <braidaniel2001@gmail.com>
2023-07-29 23:29:15 +0100

    chore: Update site link

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 import type { SocialObjects } from "./types";
 export const SITE_TITLE = 'Daniel Brai';
-export const SITE_LINK = 'https://danielbrai.ninja/'
+export const SITE_LINK = 'https://danielbrai.me/'
 export const SITE_CDN = 'https://cdn.jsdelivr.net/gh/Daniel-Brai/CDN/portfolio/'
 export const SITE_DESCRIPTION = "Daniel Brai - An Aspiring Software Engineer and third year Computer Science Undergraduate";
 export const POSTS_PER_PAGE: number = 4;
